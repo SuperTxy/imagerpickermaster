83e5a8769f665091654af14bf3f3d3a4688e6cd8
Tan Xiaoyan <tanxiaoyan@renyibang.com>
2017-09-18 16:25:21 +0800

    滑动切换视频拍照的修改完成

--- ImagePickerMaster/src/main/java/com/example/apple/glidetest/media/SlideHolder.kt
+++ ImagePickerMaster/src/main/java/com/example/apple/glidetest/media/SlideHolder.kt
@@ -1,123 +1,73 @@
 package com.example.apple.glidetest.media
 
-import android.animation.Animator
-import android.animation.ObjectAnimator
-import android.animation.ValueAnimator
 import android.graphics.Color
 import android.support.v4.content.ContextCompat
 import android.view.MotionEvent
 import android.view.View
+import android.widget.LinearLayout
 import com.example.apple.glidetest.R
 import com.example.apple.glidetest.RecordMediaActivity
 import com.orhanobut.logger.Logger
+import com.txy.androidutils.ScreenUtils
 import kotlinx.android.synthetic.main.slide_view.view.*
 
 /**
  * Created by Apple on 17/9/14.
  */
 
-class SlideHolder(private var view: View) : Animator.AnimatorListener, View.OnTouchListener {
+class SlideHolder(private var view: View) : View.OnTouchListener {
 
     private var downX = 0f
-    private var lastX = 0f
-    private var leftPos = 0
-    private var rightPos = 0
-    private var halfRedWidth = 0
     var isRedLeft: Boolean = true
 
     init {
-        view.addOnLayoutChangeListener { v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom ->
-            if (leftPos == 0) {
-                val widthTv = view.tvCamera.width
-                leftPos = view.tvCamera.left + widthTv / 2
-                rightPos = view.tvVideo.left + widthTv / 2
-                halfRedWidth = view.viewRed.width / 2
-//                Logger.e(leftPos.toString() + "------->" + rightPos.toString() + "---->" + halfRedWidth)
-                switchStatus()
-            }
-        }
+        switchStatus()
         view.tvCamera.setOnClickListener {
             if (!isRedLeft) {
-                startAnim(rightPos, leftPos)
                 isRedLeft = true
                 switchStatus()
-                if (view.context is RecordMediaActivity){
-                    (view.context  as RecordMediaActivity).changeMediaType(isRedLeft)
+                if (view.context is RecordMediaActivity) {
+                    (view.context as RecordMediaActivity).changeMediaType(isRedLeft)
                 }
             }
         }
         view.tvVideo.setOnClickListener {
             if (isRedLeft) {
-                startAnim(leftPos, rightPos)
                 isRedLeft = false
                 switchStatus()
-                if (view.context is RecordMediaActivity){
-                    (view.context  as RecordMediaActivity).changeMediaType(isRedLeft)
+                if (view.context is RecordMediaActivity) {
+                    (view.context as RecordMediaActivity).changeMediaType(isRedLeft)
                 }
             }
         }
-        view.llSlide.setOnTouchListener(this)
+        view.setOnTouchListener(this)
     }
 
     override fun onTouch(v: View?, event: MotionEvent?): Boolean {
         when (event!!.action) {
             MotionEvent.ACTION_DOWN -> {
                 downX = event.rawX
-                lastX = downX
                 Logger.d("ACTION_DOWN" + downX)
             }
-            MotionEvent.ACTION_MOVE -> {
-                val offsetX = lastX - event.rawX
-                if ((offsetX < 0 && !isRedLeft) || (offsetX > 0 && isRedLeft)) {
-                    move(offsetX.toInt())
-                    lastX = event.rawX
-                    Logger.d("ACTION_MOVE" + offsetX)
-                }
-            }
             MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-                val redCenterX = view.viewRed.left + halfRedWidth / 2
-                if (redCenterX > (leftPos + rightPos) / 2) {
-                    startAnim(redCenterX, rightPos)
-                    isRedLeft = false
+                var upX = event.rawX
+                if (upX - downX < 0) {
+                    if (isRedLeft) {
+                        isRedLeft = false
+                        switchStatus()
+                    }
                 } else {
-                    startAnim(redCenterX, leftPos)
-                    isRedLeft = true
+                    if (!isRedLeft) {
+                        isRedLeft = true
+                        switchStatus()
+                    }
                 }
-                switchStatus()
                 Logger.d("ACTION_UP" + isRedLeft)
             }
         }
         return true
     }
 
-    private fun startAnim(startX: Int, endX: Int) {
-        val animator = ObjectAnimator.ofInt(view.viewRed, "translationX", startX, endX)
-        val duration = Math.abs(endX - startX) / (rightPos - leftPos) * 200L
-        animator.duration = duration
-        animator.addUpdateListener(object : ValueAnimator.AnimatorUpdateListener {
-            override fun onAnimationUpdate(animation: ValueAnimator?) {
-            }
-
-        })
-        animator.start()
-    }
-
-    private fun move(offsetX: Int) {
-        var offset = offsetX
-        val redCenterPos = view.viewRed.left + halfRedWidth
-        if (redCenterPos >= leftPos && redCenterPos <= rightPos) {
-            if (redCenterPos + offsetX < leftPos)
-                offset = leftPos - redCenterPos
-            if (redCenterPos + offsetX > rightPos)
-                offset = rightPos - redCenterPos
-            view.viewRed.layout(view.viewRed.left + offset, view.viewRed.top, view.viewRed.bottom, view.viewRed.right + offset)
-            view.viewRed.requestLayout()
-        }
-    }
-
-    override fun onAnimationEnd(animation: Animator?) {
-        switchStatus()
-    }
 
     fun switchStatus() {
         view.tvCamera.visibility = View.VISIBLE
@@ -128,21 +78,11 @@ class SlideHolder(private var view: View) : Animator.AnimatorListener, View.OnTo
         view.tvVideo.setTextColor(selectedColor(!isRedLeft))
         view.tvCamera.invalidate()
         view.tvVideo.invalidate()
-        val pos = if (isRedLeft) leftPos else rightPos
         view.viewRed.visibility = View.VISIBLE
-        view.viewRed.layout(pos - halfRedWidth, view.viewRed.top, pos + halfRedWidth, view.viewRed.bottom)
-    }
-
-    override fun onAnimationStart(animation: Animator?) {
-        Logger.e("onAnimationStart")
-    }
-
-    override fun onAnimationRepeat(animation: Animator?) {
-        Logger.e("onAnimationRepeat")
-    }
-
-    override fun onAnimationCancel(animation: Animator?) {
-        Logger.e("onAnimationCancel")
+        val params = view.viewRed.layoutParams as LinearLayout.LayoutParams
+        params.rightMargin = if (isRedLeft) ScreenUtils.dp2px(view.context, 20) else 0
+        params.leftMargin = if (isRedLeft) 0 else ScreenUtils.dp2px(view.context, 20)
+        view.viewRed.layoutParams = params
 
     }
 
